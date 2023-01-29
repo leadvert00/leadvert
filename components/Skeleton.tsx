9613e2e0189d5e0afaebe1eeda917cf5d7e00483
Bello Samuel <prinxsabelo@gmail.com>
2023-01-29 01:46:57 +0100

    ..

--- components/Skeleton.tsx
+++ components/Skeleton.tsx
@@ -1,13 +1,7 @@
 export default function Skeleton() {
   return (
     <div>
-      <div className="skeleton">
-        <div className="s-banner"></div>
-        <div className="s-header"></div>
-        <div className="s-content"></div>
-        <div className="s-content"></div>
-        <div className="s-content"></div>
-      </div>
+      <div className="skeleton"></div>
     </div>
   );
 }
