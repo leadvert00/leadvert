6a897f71cbc0f2f3d6438d2749eee13d1f6a724a
Bello Samuel <prinxsabelo@gmail.com>
2023-01-29 14:23:46 +0100

    ..

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -48,7 +48,8 @@ const BlogCard = ({ blog }: any) => {
             <div className="flex items-center space-x-2 ">
               {publishDate && (
                 <div className="text-sm ">
-                  {Moment(publishDate).format('MM-DD-YYY')}
+                  {new Date(publishDate).toString()}
+                  {/* {Moment(publishDate).format('MM-DD-YYY')} */}
                   {/* <Moment format="MMMM D, YYYY"Moment>{dateToFormat}</Moment> */}
                 </div>
               )}
